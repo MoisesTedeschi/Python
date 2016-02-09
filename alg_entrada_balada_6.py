b2acc03d3c7e5f670bef393458e0a474fca78333
MoisesTedeschi <moisestedeschi@gmail.com>
2016-02-09 14:57:29 -0200

    Algoritmo usando entrada de dados e condição simples.

---
+++ alg_entrada_balada_6.py
@@ -0,0 +1,13 @@
+"""
+Algoritmo entrada em balada:
+Entrada idade de uma pessoa;
+Se a idade for maior ou igual a 18 anos a pessoa terá acesso liberado.
+"""
+
+print("Seja bem-vindo ao X Festival de Cerveja na Região Sudeste")
+
+idade = int(input("Entre com sua idade: "))
+if (idade >= 18):
+      print("Sua entrada está liberada.\nDivista-se!!!")
+else:
+      print("Lamento, mas sua entrada não foi liberada!")
